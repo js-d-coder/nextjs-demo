0973aa5ac8c477a2b7641eb1dd61ad6e709b14e2
Jibran Shaikh <23054879+js-d-coder@users.noreply.github.com>
2024-02-19 20:21:16 +0530

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { defaultFont } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard | Jibran Shaikh',
+  description: 'Demo app showcasing the application of Next.js, React.js, PostgreSQL.',
+  metadataBase: new URL('https://nextjs-demo-jade-omega.vercel.app'),
+};
 
 export default function RootLayout({
   children,
